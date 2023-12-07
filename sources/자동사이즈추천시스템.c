46f9b26c7035b0d1bee77c67315c8c16875de54a
cksals0316 <cksals7656@naver.com>
2023-12-07 17:58:50 +0900

    개인 프로젝트 수정본

--- sources/자동사이즈추천시스템.c
+++ sources/자동사이즈추천시스템.c
@@ -1,16 +1,27 @@
 #include <stdio.h>
+#include <string.h>
+
+// 구조체 정의: 고객 신체 정보
+struct CustomerInfo {
+    float height;
+    float weight;
+    float waistCircumference;
+};
+
+// 구조체 정의: 이전 구매 내역
+struct PurchaseHistory {
+    char itemName[50];
+    float itemPrice;
+};
 
 int main() {
     // 이전 구매 내역 초기화
-    char previousItemName[50];
-    float previousItemPrice;
-
-    // 흑청바지 정보 입력
-    strcpy(previousItemName, "흑청바지");
-    previousItemPrice = 35000.0;
+    struct PurchaseHistory previousPurchase;
+    strcpy_s(previousPurchase.itemName, sizeof(previousPurchase.itemName), "흑청바지");
+    previousPurchase.itemPrice = 35000.0;
 
     // 변수 선언
-    float height, weight, waistsize;
+    float height, weight, waistCircumference;
 
     // 사용자로부터 키 입력 받기 (양수여야 함)
     printf("키를 입력하세요 (양수, cm): ");
@@ -30,8 +41,8 @@ int main() {
 
     // 사용자로부터 허리둘레 입력 받기 (양수여야 함)
     printf("허리둘레를 입력하세요 (양수, cm): ");
-    scanf_s("%f", &waistsize);
-    if (waistsize <= 0) {
+    scanf_s("%f", &waistCircumference);
+    if (waistCircumference <= 0) {
         printf("잘못된 입력입니다. 허리둘레는 양수여야 합니다.\n");
         return 1; // 프로그램 종료
     }
@@ -40,12 +51,12 @@ int main() {
     printf("\n고객 신체 정보\n");
     printf("키: %.2f cm\n", height);
     printf("몸무게: %.2f kg\n", weight);
-    printf("허리둘레: %.2f cm\n", waistsize);
+    printf("허리둘레: %.2f cm\n", waistCircumference);
 
     // 이전 구매 내역 출력
     printf("\n이전 구매 내역\n");
-    printf("아이템명: %s\n", previousItemName);
-    printf("가격: %.2f 원\n", previousItemPrice);
+    printf("아이템명: %s\n", previousPurchase.itemName);
+    printf("가격: %.2f 원\n", previousPurchase.itemPrice);
 
     return 0;
 }
